3c6bc3945539a24d4d943a050080b1205a8b5877
1098673338 <1098673338@users.noreply.github.com>
2026-05-14 01:50:04 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -31,7 +31,7 @@ export async function middleware(request: Request) {
     }
 
     const session = await auth()
-    if (!session?.user) {
+    if (!session?.user?.id) {
       return NextResponse.json(
         { error: "未授权" },
         { status: 401 }
