dfb4bc4eb28263921f84f63237ed0702b3ef5e4b
1098673338 <1098673338@qq.com>
2026-05-14 14:57:52 +0800

    Update header.tsx

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -5,7 +5,7 @@ import { Logo } from "@/components/ui/logo"
 export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 h-16 min-w-[1920px]">
-      <div className="h-full w-full px-6">
+      <div className="h-full w-full pl-8 pr-5">
         <div className="h-full flex items-center justify-between">
           <Logo />
           <div className="flex items-center gap-y-4 gap-x-4">
