812cf75d56e9a9023ce3a1a2f30fe9981a3c65fe
1098673338 <1098673338@users.noreply.github.com>
2026-05-16 15:31:55 +0800

    Update send-dialog.tsx

--- app/components/emails/send-dialog.tsx
+++ app/components/emails/send-dialog.tsx
@@ -97,7 +97,7 @@ export function SendDialog({ emailId, fromAddress, onSendSuccess }: SendDialogPr
                 size="sm"
                 className="h-8 gap-1"
               >
-                <Send className="h-3.5 w-3.5" />
+                <Send className="h-4 w-4" />
                 <span className="hidden sm:inline">{t("trigger")}</span>
               </Button>
             </TooltipTrigger>
